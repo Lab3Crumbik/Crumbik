473168f12b93b9a144b58e023df305b1d59e424c
noreysi <noreysiescalona@gmail.com>
2016-04-01 12:27:42 -0430

    validaciones en el formulario de pago listo

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,6 @@
 class Payment < ActiveRecord::Base
+	validates  :card_number, length:{is:16} 
+    validates :card_number, :expiration_month, :amount, presence: {message:"Obligatorio"}
+    
 	 
 end
