b981b09c06827208d4d65eb382b4de37879ca94b
rdgc91 <rdgc91@gmail.com>
2016-04-03 13:04:33 -0430

    Diseno en las vistas productos, pago y mailer

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,13 +1,11 @@
 class CampaignMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.campaign_mailer.new_campaign.subject
-  #
-  def new_campaign(user)
-
-
-    mail(to: user.email, subject:'Nueva campaña')
+  def new_campaign(user, advertising_campaign)
+    @campa = advertising_campaign
+    @r= @campa.avatar.url
+    attachments.inline["image.png"] = File.read("#{Rails.root}"+"/"+"public"+"/"+@r.at(1..45)+@campa.avatar_file_name)
+    mail(to: user.email, subject:@campa.name)
   end
 end
+
+
